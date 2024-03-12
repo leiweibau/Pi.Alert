23c073e0b9b6eea6419ad1040107c6a911bcd419
leiweibau <105860611+leiweibau@users.noreply.github.com>
2024-03-12 13:03:30 +0100

    Update pialert_install.sh

--- install/pialert_install.sh
+++ install/pialert_install.sh
@@ -692,7 +692,6 @@ check_pialert_home() {
 install_dependencies() {
   print_msg "- Installing dependencies..."
   if [ $(id -u) -eq 0 ]; then
-      apt-get update                                             2>&1 >> "$LOG"
       apt-get install sudo -y                                    2>&1 >> "$LOG"
   fi
 
