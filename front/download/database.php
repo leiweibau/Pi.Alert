afe287fa538a65c3e64a2ca15e0fe40071ac4f9e
leiweibau <105860611+leiweibau@users.noreply.github.com>
2024-06-20 12:23:17 +0200

    Securing download scripts

--- front/download/database.php
+++ front/download/database.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+
+if ($_SESSION["login"] != 1) {
+	header('Location: ../index.php');
+	exit;
+}
+
 $ARCHIVE_PATH = str_replace('download', '', str_replace('front', 'db', getcwd()));
 $LATEST_FILES = glob($ARCHIVE_PATH . "pialertdb_*.zip");
 if (sizeof($LATEST_FILES) == 0) {
