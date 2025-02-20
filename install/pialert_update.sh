7606059b629f500d2a833efa311c4c23e18d09ad
leiweibau <105860611+leiweibau@users.noreply.github.com>
2025-02-20 20:18:21 +0100

    Pihole 6 support 08
    #452

--- install/pialert_update.sh
+++ install/pialert_update.sh
@@ -59,7 +59,7 @@ main() {
   print_header "Update process finished"
   if $PIHOLE_MOVED ; then
     print_msg ""
-    print_msg "!! Pi-hole Webinterface moved from Port 80 to Port 8080 !!"
+    print_header "!! Pi-hole Webinterface moved from Port 80 to Port 8080 !!"
   fi
   print_msg ""
 
@@ -125,13 +125,13 @@ check_pihole() {
         CORE_VERSION=$(echo "$VERSION_OUTPUT" | grep -oP 'Core version is v\K[0-9]+')
 
         if [[ $CORE_VERSION -ge 6 ]]; then
-            echo "Pi-hole 6.x detected. Webinterface moved to Port 8080..."
+            print_header "Pi-hole 6.x detected. Webinterface moved to Port 8080..."
             sudo systemctl stop lighttpd
             sudo systemctl start lighttpd
             sudo pihole-FTL --config webserver.port 8080o,[::]:8080o,443so,[::]:443so
             sudo systemctl restart pihole-FTL
             sudo systemctl start lighttpd
-            echo "Pi-hole Configuration applied"
+            #echo "Pi-hole Configuration applied"
             PIHOLE_MOVED=true
         fi
     fi
