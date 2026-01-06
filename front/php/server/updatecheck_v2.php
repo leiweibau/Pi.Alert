ba9d17c7b9ebcb5c2fa7f95d77d57f4cdf4c5aac
leiweibau <105860611+leiweibau@users.noreply.github.com>
2026-01-06 15:20:29 +0100

    Change update command to use sudo
    regarding to #558
    thx to @lastsamurai26 for pointing this out

--- front/php/server/updatecheck_v2.php
+++ front/php/server/updatecheck_v2.php
@@ -251,7 +251,7 @@ if ($pialert_cur_version != $pialert_new_version && $valid_update_notes) {
 		}
 	}
 	if (!file_exists("/opt/pialert")) {
-		$updatecommand = 'bash -c &quot;$(wget -qLO - https://github.com/leiweibau/Pi.Alert/raw/main/install/pialert_update_old.sh)&quot;';
+		$updatecommand = 'sudo bash -c &quot;$(wget -qLO - https://github.com/leiweibau/Pi.Alert/raw/main/install/pialert_update_old.sh)&quot;';
 		$updateenv = ' (Outdated)';
 	} else {
 		$updatecommand = 'bash -c &quot;$(curl -fsSL https://github.com/leiweibau/Pi.Alert/raw/main/install/pialert_update.sh)&quot; -s';
